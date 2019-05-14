281568b28fab15100359985a2bc7c30d33ec1564
Your Name <you@example.com>
2019-05-14 16:00:38 +0900

    20190514-1

--- cf/create_cf.py
+++ cf/create_cf.py
@@ -4,91 +4,64 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 
-# パスの指定
+# path
 base_dir = "."
 
 print("\n\n---------- START ----------")
 
 #-----------------------------------------------------
-# パラメータの初期化
+# parameter
 #-----------------------------------------------------
 user_min = 20
 k= 35
 
 #-----------------------------------------------------
-# 引数が渡された場合の処理
-#-----------------------------------------------------
-args = sys.argv
-if len(args) == 2:
-    user_min = args[1]
-elif len(args) == 3:
-    user_min = args[1]
-    k = args[2]
-#else:
-    #print('\n以下のような形式で[user_min]と[k]を指定してください')
-    #print('python3 \nmf_test.py 20 35 \n')
-
-user_min = int(user_min)
-k = int(k)
-
-print("user_min = " + str(user_min))
-print("k = " + str(k))
-print()
-
-#-----------------------------------------------------
-# CSVファイルをpandas.DataFrame に読み込む
+# Load CSV file into pandas.DataFram
 #-----------------------------------------------------
 df = pd.read_csv(base_dir + '/data/score.csv')
-#print(df)
-#sys.exit()
-
 
 #-----------------------------------------------------
-# 対象データの整形
+# Reshape the data
 #-----------------------------------------------------
-# id 列をインデックスに指定
+# Setid column as index
 df_i = df.set_index('id')
-#print(df_i.index)
 
-# [id]指定した本数以上のユーザーのみ(1以上の要素を true として合算する)
+# Sum one or more elements as true
 df_bool = (df_i > 0)
-#print(df_bool)
-#print(df_bool.sum(axis=1))
+
+# Users who watched more than the specified number
 indexer_user = df_bool.sum(axis=1) >= user_min
 df_i_user = df_i[indexer_user]
 print(df_i_user)
 
-# [講義ID]全てが0の列を除去
+# Remove columns with all values 0
 indexer_movie_id = df_i_user.sum(axis=0) > 0
 df_target = df_i_user[indexer_movie_id.index[indexer_movie_id]]
 print(df_target)
 
 #-----------------------------------------------------
-# データ妥当性のチェック
+# Check data validity
 #-----------------------------------------------------
-# 0のみの行の検知
+# Detect rows with all values 0
 print("#####################################")
 indexer_zero = df_target.sum(axis=1) == 0
 print(df_target[indexer_zero])
 print(len(df_target[indexer_zero]))
 print()
 
-# 0のみの列の検知
+# Detect columns with all values 0
 indexer_zero = df_target.sum(axis=0) == 0
 print(df_target[indexer_zero.index[indexer_zero]])
 print(len(df_target[indexer_zero.index[indexer_zero]].columns))
 
 #-----------------------------------------------------
-# カラムとインデックスの保持
+# Keep columns and indexes
 #-----------------------------------------------------
 score_columns = df_target.columns
 score_index = df_target.index
 
-#print(score_columns)
-#print(score_index)
-
 #-----------------------------------------------------
-# nmf 開始
+# nmf
 #-----------------------------------------------------
 print("\n*************************")
 print("START nmf")
@@ -110,13 +83,13 @@ def update(Y, W, H, mask):
     H = H * np.dot(W.T, Y * mask) / np.dot(W.T, np.dot(W, H) * mask)
     return W, H
 
-# WとHの更新を繰り返してWとHの行列の積を徐々にYに近づけていく
+# Repeat W and H updates and Make the product of W and H matrices gradually closer Y
 for _ in range(100):
     W, H = update(Y, W, H, mask_Y)
     print(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 
 #-----------------------------------------------------
-# CSVファイルへ出力
+# save to CSV file
 #-----------------------------------------------------
 nm_csv_file = base_dir + "/data/nmf_score_" + str(user_min) + "_"  + str(k)  +".csv"
 
@@ -127,7 +100,7 @@ print(df)
 df.to_csv(nm_csv_file)
 
 print()
-print(nm_csv_file + " を作成しました")
+print("save to " + nm_csv_file)
 
 print("\n\n---------- END ----------\n\n")
 
